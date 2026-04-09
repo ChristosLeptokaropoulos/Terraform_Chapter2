502297bdf57154cfc777af69bd256c9bf7712525
ChristosLeptokaropoulos <lepto2003@gmail.com>
2026-04-09 15:39:49 +0300

    Refactor AWS launch configuration and autoscaling group resources for clarity and consistency

--- main.tf
+++ main.tf
@@ -43,26 +43,42 @@ resource "aws_security_group" "instance" {
 }
 
 resource "aws_launch_configuration" "example" {
- image_id        = data.aws_ami.ubuntu.id
- instance_type   = "t3.micro"
- security_groups = [aws_security_group.instance.id]
- 
- user_data = <<-EOF
-             #!/bin/bash
-             echo "Hello, World" > index.xhtml
-             nohup busybox httpd -f -p ${var.server_port} &
-             EOF
+    image_id        = data.aws_ami.ubuntu.id
+    instance_type   = "t3.micro"
+    security_groups = [aws_security_group.instance.id]
+    
+    user_data = <<-EOF
+                #!/bin/bash
+                echo "Hello, World" > index.xhtml
+                nohup busybox httpd -f -p ${var.server_port} &
+                EOF
+
+    lifecycle{
+        create_before_destroy = true
+    }
 }
 
 resource "aws_autoscaling_group" "example" {
- launch_configuration = aws_launch_configuration.example.name
+    launch_configuration = aws_launch_configuration.example.name
+    vpc_zone_identifier  = data.aws_subnets.default.ids
 
- min_size = 2
- max_size = 10
- 
- tag {
-    key                 = "Name"
-    value               = "terraform-asg-example"
-    propagate_at_launch = true
- }
+    min_size = 2
+    max_size = 10
+    
+    tag {
+        key                 = "Name"
+        value               = "terraform-asg-example"
+        propagate_at_launch = true
+    }
+}
+
+data "aws_subnets" "default" { 
+    filter{
+        name = "vpc-id"
+        values = [data.aws_vpc.default.id]
+    }
+}
+
+data "aws_vpc" "default"{ 
+    default = true
 }
