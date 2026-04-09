b126c029caa41f6e41fdb8a51965873ed54abc4a
ChristosLeptokaropoulos <lepto2003@gmail.com>
2026-04-09 15:09:10 +0300

    Add launch configuration and autoscaling group resources for AWS

--- main.tf
+++ main.tf
@@ -41,3 +41,28 @@ resource "aws_security_group" "instance" {
         cidr_blocks = ["0.0.0.0/0"]
     }
 }
+
+resource "aws_launch_configuration" "example" {
+ image_id        = data.aws_ami.ubuntu.id
+ instance_type   = "t3.micro"
+ security_groups = [aws_security_group.instance.id]
+ 
+ user_data = <<-EOF
+             #!/bin/bash
+             echo "Hello, World" > index.xhtml
+             nohup busybox httpd -f -p ${var.server_port} &
+             EOF
+}
+
+resource "aws_autoscaling_group" "example" {
+ launch_configuration = aws_launch_configuration.example.name
+
+ min_size = 2
+ max_size = 10
+ 
+ tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+ }
+}
